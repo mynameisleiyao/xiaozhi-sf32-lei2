67a7a1628c329e23f84e9fd2bb4b2c64fe5828cc
yaoleikirin <10116253+Ginkgo_leaf@user.noreply.gitee.com>
2025-09-06 21:23:38 +0800

    删除ST7789 SPI 外设

--- export.ps1环境配置 - sf32_sdk.bat
+++ export.ps1环境配置 - sf32_sdk.bat
@@ -1,3 +1,2 @@
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe  -ExecutionPolicy Bypass -NoExit -File  E:\Desktop\ProjectJamal\SF32_GITEE\SDK\sifli-sdk\export.ps1
-
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe  -ExecutionPolicy Bypass -NoExit -File  G:\Desktop\ProjectJamal\SF32_GITEE\SDK\sifli-sdk\export.ps1
 pause
